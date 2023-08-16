7fbb7afeecdb9b6f0b0d6b537b6f0df867d74ff9
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-08-16 01:10:11 -0300

    Adjusts
    Adjusted Card infos and layout

--- src/components/Restaurants/RestaurantCard/Card.tsx
+++ src/components/Restaurants/RestaurantCard/Card.tsx
@@ -2,6 +2,7 @@ import classes from "./Card.module.css";
 import starIcon from "../../../images/star_icon.svg";
 import badStarIcon from "../../../images/low_star_icon.svg";
 import timeIcon from "../../../images/delivery_icon.svg";
+import locationIcon from "../../../images/location_icon.svg";
 import { Link } from "react-router-dom";
 
 interface CardProps {
@@ -12,19 +13,28 @@ interface CardProps {
   location: string;
 }
 
-const Card: React.FC<CardProps> = ({ name, rating, deliveryTime, link }) => {
+const Card: React.FC<CardProps> = ({
+  name,
+  rating,
+  deliveryTime,
+  link,
+  location,
+}) => {
   const imageRandom = Math.floor(Math.random() * 4) + 1; // so pra aleatorizar a imagem, já que não foi hospedada na api
   const imageSrc = `https://source.unsplash.com/256x256/?food,00${imageRandom}`;
 
   return (
     <>
       <div className={classes.card}>
-        <img src={imageSrc} alt="dish" />
+        <img src={imageSrc} className={classes.card_image} alt="dish" />
         <Link to={`/restaurant/${link}`} className={classes.link}>
           <span className={classes.card_title}>{name}</span>
         </Link>
         <div className={classes.card_about}>
-          <span className={classes.card_cat}>something</span>
+          <span className={classes.card_location}>
+            <img src={locationIcon} alt="location" />
+            {location}
+          </span>
           <span className={classes.card_stars_delivery}>
             {rating >= 4.5 ? (
               <img src={starIcon} alt="rating" />
