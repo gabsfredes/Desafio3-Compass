e72203920e2886d095faded0470107a82ae6234e
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-08-16 02:06:03 -0300

    Added Transition
    Added pages transitions effect to routes

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,15 +1,27 @@
+import React, { useState, useEffect } from "react";
 import ScrollToTop from "../ScrollToTop";
 import Error from "../components/Error/Error";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import "../App.css";
 
 const ErrorPage = () => {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeIn(true);
+    }, 100);
+  }, []);
+
   return (
     <>
-      <ScrollToTop />
-      <Header />
-      <Error />
-      <Footer />
+      <div className={fadeIn ? "fade-in active" : "fade-in"}>
+        <ScrollToTop />
+        <Header />
+        <Error />
+        <Footer />
+      </div>
     </>
   );
 };
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ScrollToTop from "../ScrollToTop";
+import "../App.css";
 
 const Root: React.FC = () => {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeIn(true);
+    }, 100);
+  }, []);
+
   return (
     <>
-      <Header />
-      <ScrollToTop />
-      <Outlet />
-      <Footer />
+      <div className={fadeIn ? 'fade-in active' : 'fade-in'}>
+        <Header />
+        <ScrollToTop />
+        <Outlet />
+        <Footer />
+      </div>
     </>
   );
 };
